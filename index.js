21e89a92ae59fa19226961116682c7813dcd1e43
dalazaca <diezucchet@gmail.com>
2017-09-18 06:00:53 -0500

    Build arreglado

--- index.js
+++ index.js
@@ -8,17 +8,9 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
 
   // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
+  res.send('hello world');
 });
 
 // The "catchall" handler: for any request that doesn't
